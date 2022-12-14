2ae5ba4f812161a183760f1e6d2cd8eb78f364b4
PedroHLongo <pedro.longo@bradescoseguros.com.br>
2022-12-13 21:03:13 -0300

    dependency inversion

--- src/application/useCases/sendNotification.spec.ts
+++ src/application/useCases/sendNotification.spec.ts
@@ -1,17 +1,26 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './sendNotification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recepientId: 'example-recepient-id',
     });
 
-    //Persistir essa notificação no banco
+    console.log(notifications);
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
